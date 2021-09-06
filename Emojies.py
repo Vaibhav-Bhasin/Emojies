c090e99da4ac9a72ef21ef902df2bf5c418a2145
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-06 22:21:34 +0530

    Update Emojies.py

--- Emojies.py
+++ Emojies.py
@@ -1,1 +1,17 @@
+# grinning face
+print("\U0001f600")
+
+# grinning squinting face
+print("\U0001F606")
+
+# rolling on the floor laughing
+print("\U0001F923")
+# grinning face
+print("\N{grinning face}")
+
+# slightly smiling face
+print("\N{slightly smiling face}")
+
+# winking face
+print("\N{winking face}")
 

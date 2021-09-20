675347914a876b4f5c9f220c6e95266e3d3ce969
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-20 21:53:22 +0530

    Update Unicode_Of_Emojies.py

--- Unicode_Of_Emojies.py
+++ Unicode_Of_Emojies.py
@@ -23,9 +23,9 @@ kissing face with smiling eyes	                                            :)  U
 face savoring food	                                                      :)  U+1F60B  (:
 face with tongue	                                                        :)  U+1F61B  (:
 winking face with tongue                                                          :)  U+1F61C  (:
-zany face                         :)  U+1F92A  (: 
-squinting face with tongue        :)  U+1F61D  (:
-money-mouth face	          :)  U+1F911  (:
+zany face                                                                           :)  U+1F92A  (: 
+squinting face with tongue                                                        :)  U+1F61D  (:
+money-mouth face	                                                        :)  U+1F911  (:
 hugging face                      :)  U+1F917  (:
 face with hand over mouth         :)  U+1F92D  (:
 shushing face	                  :)  U+1F92B  (:

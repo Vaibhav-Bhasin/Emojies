45148b59fcb5a64886f02b98783e8ffaf209beff
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-21 20:38:56 +0530

    Update Unicode_Of_Emojies.py

--- Unicode_Of_Emojies.py
+++ Unicode_Of_Emojies.py
@@ -1,4 +1,4 @@
-#Unicode                          #Unicode
+#Unicode                      :    #Unicode
 grinning face	                    :)  U+1F600  (:
 grinning face with big eyes           :)  U+1F603  (:
 grinning face with smiling eyes         :)  U+1F604  (:
@@ -30,12 +30,12 @@ hugging face                                                                 :) 
 face with hand over mouth                                                 :)  U+1F92D  (:
 shushing face	                                                       :)  U+1F92B  (:
 thinking face                                                        :)  U+1F914  (:
-zipper-mouth face                 :)  U+1F910  (:
-face with raised eyebrow          :)  U+1F928  (:
-neutral face	                  :)  U+1F610  (:
-expressionless face	          :)  U+1F611  (:
-face without mouth	          :)  U+1F636  (:
-smirking face	                  :)  U+1F60F  (:
+zipper-mouth face                                                   :)  U+1F910  (:
+face with raised eyebrow                                          :)  U+1F928  (:
+neutral face	                                                 :)  U+1F610  (:
+expressionless face	                                        :)  U+1F611  (:
+face without mouth	                                      :)  U+1F636  (:
+smirking face	                                            :)  U+1F60F  (:
 unamused face	                                          :)  U+1F612  (:
 face with rolling eyes                                  :)  U+1F644  (:
 grimacing face                                        :)  U+1F62C  (:

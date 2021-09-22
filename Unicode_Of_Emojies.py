43aa05fdded1569bc9e8995fbd59e06c2011e86a
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-22 22:01:36 +0530

    Update Unicode_Of_Emojies.py

--- Unicode_Of_Emojies.py
+++ Unicode_Of_Emojies.py
@@ -30,19 +30,19 @@ hugging face                    :                                               
 face with hand over mouth       :                                           :)  U+1F92D  (:
 shushing face	                :                                       :)  U+1F92B  (:
 thinking face                   :                                     :)  U+1F914  (:
-zipper-mouth face                :                                   :)  U+1F910  (:
-face with raised eyebrow          :                                :)  U+1F928  (:
-neutral face	                   :                              :)  U+1F610  (:
-expressionless face	            :                          :)  U+1F611  (:
-face without mouth	             :                         :)  U+1F636  (:
-smirking face	                      :                      :)  U+1F60F  (:
-unamused face	                       :                   :)  U+1F612  (:
-face with rolling eyes                  :                :)  U+1F644  (:
-grimacing face                           :           :)  U+1F62C  (:
-lying face	                          :          :)  U+1F925  (:
-relieved face	                           :       :)  U+1F60C  (:
-pensive face                                :    :)  U+1F614  (:
-sleepy face                                  : :)  U+1F62A  (:
+zipper-mouth face               :                                   :)  U+1F910  (:
+face with raised eyebrow        :                                :)  U+1F928  (:
+neutral face	                :                              :)  U+1F610  (:
+expressionless face	        :                          :)  U+1F611  (:
+face without mouth	        :                         :)  U+1F636  (:
+smirking face	                :                      :)  U+1F60F  (:
+unamused face	                :                   :)  U+1F612  (:
+face with rolling eyes          :                :)  U+1F644  (:
+grimacing face                  :           :)  U+1F62C  (:
+lying face	                :          :)  U+1F925  (:
+relieved face	                :       :)  U+1F60C  (:
+pensive face                    :    :)  U+1F614  (:
+sleepy face                     : :)  U+1F62A  (:
 drooling face          :                     :)  U+1F924  (:
 sleeping face           :  	          :)  U+1F634  (:
 face with medical mask   :               :)  U+1F637  (:

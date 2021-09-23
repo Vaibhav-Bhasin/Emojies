8ccb15807f6a11939b84df5a48b286bfb1d254cc
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-23 21:30:51 +0530

    Update Unicode_Of_Emojies.py

--- Unicode_Of_Emojies.py
+++ Unicode_Of_Emojies.py
@@ -13,12 +13,12 @@ winking face	                :                     :)  U+1F609  (:
 smiling face with smiling eyes  :                       :)  U+1F60A  (:
 smiling face with halo	        :                         :)  U+1F607  (:
 smiling face with 3 hearts      :                           :)  U+1F970  (:
-smiling face with heart-eyes    :                                    :)  U+1F60D  (:
-star-struck                     :                                     :)  U+1F929  (:
+smiling face with heart-eyes    :                             :)  U+1F60D  (:
+star-struck                     :                               :)  U+1F929  (:
 face blowing a kiss	        :                                 :)  U+1F618  (:
-kissing face	                :                                    :)  U+1F617  (:
-smiling face	                :                                        :)  U+263A   (:
-kissing face with closed eyes   :                                         :)  U+1F61A  (:
+kissing face	                :                                   :)  U+1F617  (:
+smiling face	                :                                     :)  U+263A   (:
+kissing face with closed eyes   :                                       :)  U+1F61A  (:
 kissing face with smiling eyes  :                                          :)  U+1F619  (:
 face savoring food	        :                                           :)  U+1F60B  (:
 face with tongue	        :                                        :)  U+1F61B  (:

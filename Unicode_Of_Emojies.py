a180c1e783ba36993883a034cac53af28b3bed14
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-23 21:44:05 +0530

    Update Unicode_Of_Emojies.py

--- Unicode_Of_Emojies.py
+++ Unicode_Of_Emojies.py
@@ -45,7 +45,7 @@ pensive face                    :             :)  U+1F614  (:
 sleepy face                     :           :)  U+1F62A  (:
 drooling face                   :         :)  U+1F924  (:
 sleeping face                   :       :)  U+1F634  (:
-face with medical mask          :               :)  U+1F637  (:
+face with medical mask          :     :)  U+1F637  (:
 face with thermometer           :            :)  U+1F912  (:
 face with head-bandage          :         :)  U+1F915  (:
 nauseated face	                ::)  U+1F922  (:
